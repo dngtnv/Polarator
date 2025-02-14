54494dc5909822eeefb0ecc4f78f0dc93dc8dd0c
dngtnv <kakalupin@gmail.com>
2025-02-14 17:39:03 +0700

    init commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="px-4 py-5">
+        <div className="flex items-center font-bold">
+          <div className="rounded-xl border-2 border-black px-2 py-1">
+            Polarator
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,14 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
     extend: {
       colors: {
         background: "var(--background)",
